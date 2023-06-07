921c703009ff405d3eae5d4e04000b5bc2a69d69
hendisantika <hendisantika@yahoo.co.id>
2023-06-07 09:54:11 +0700

    #1 - Init project ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootcqrs/SpringBootCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCqrsApplication.class, args);
+    }
+
+}
