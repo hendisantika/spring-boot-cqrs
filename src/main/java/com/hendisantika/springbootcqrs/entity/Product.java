c9217de6385051d25507012859928da3dfa18b77
hendisantika <hendisantika@yahoo.co.id>
2023-06-07 10:09:34 +0700

    #4 - Add Product Entity ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootcqrs/entity/Product.java
@@ -0,0 +1,31 @@
+package com.hendisantika.springbootcqrs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/7/23
+ * Time: 10:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private double price;
+}
