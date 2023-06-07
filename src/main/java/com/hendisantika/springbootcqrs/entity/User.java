f3923af90371ecf11877a25fbed526972395effa
hendisantika <hendisantika@yahoo.co.id>
2023-06-07 10:11:34 +0700

    #7 - Add User Entity ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootcqrs/entity/User.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootcqrs.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/7/23
+ * Time: 10:11
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String state;
+}
