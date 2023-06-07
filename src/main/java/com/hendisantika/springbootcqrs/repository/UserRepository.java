ab1bcb659887077f4a8ad6df401561531ec4f17c
hendisantika <hendisantika@yahoo.co.id>
2023-06-07 10:15:31 +0700

    #13 - Add UserRepository ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootcqrs/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootcqrs.repository;
+
+import com.hendisantika.springbootcqrs.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/7/23
+ * Time: 10:15
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
